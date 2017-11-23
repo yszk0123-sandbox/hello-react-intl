c82a2b06fe8d0c34a31508e65b3923bfe9fd3b8b
Yuji Suzuki <y0suzuki0123@gmail.com>
2017-11-24 04:31:53 +0900

    feat: introduce Polyglot.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ import { IntlProvider } from 'react-intl';
 import App from './components/App';
 import configureI18next from './configureI18next';
 import { I18nProvider } from 'lingui-react';
+import Polyglot from 'node-polyglot';
 
 configureI18next();
+const polyglot = new Polyglot();
+console.log(polyglot.t('a'));
 
 render(
   <I18nProvider>
