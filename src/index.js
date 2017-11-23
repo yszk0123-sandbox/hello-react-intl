3bc1123687fc043eecf2528670d2e96a7ffe497c
Yuji Suzuki <y0suzuki0123@gmail.com>
2017-11-24 04:23:19 +0900

    feat: introduce js-lingui

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import { render } from 'react-dom';
 import { IntlProvider } from 'react-intl';
 import App from './components/App';
 import configureI18next from './configureI18next';
+import { I18nProvider } from 'lingui-react';
 
 configureI18next();
 
 render(
-  <IntlProvider locale="en">
-    <App />
-  </IntlProvider>,
+  <I18nProvider>
+    <IntlProvider locale="en">
+      <App />
+    </IntlProvider>
+  </I18nProvider>,
   document.getElementById('app'),
 );
